efb3b31381faf910a32503b221d42193532d3879
Andres Gaviria T <44479766+agavariat@users.noreply.github.com>
2021-03-12 15:41:32 -0500

    Update proxy_ssl.sh

--- proxy_ssl.sh
+++ proxy_ssl.sh
@@ -16,36 +16,71 @@ upstream odoolong {
 
 server {
   server_name $dom www.$dom $oIP;
-  listen 80;
-  access_log /var/log/nginx/testing-access.log;
-  error_log /var/log/nginx/testing-error.log;
-  location /longpolling {
-  proxy_connect_timeout 3600;
-  proxy_read_timeout 3600;
-  proxy_send_timeout 3600;
-  send_timeout 3600;
-  proxy_pass http://127.0.0.1:8072;
-  }
-  location / {
-  proxy_connect_timeout 3600;
-  proxy_read_timeout 3600;
-  proxy_send_timeout 3600;
-  send_timeout 3600;
-  proxy_pass http://127.0.0.1:8069;
-  proxy_set_header Host \$host:\$server_port;
-  proxy_set_header X-Forwarded-Host \$host;
-  proxy_set_header X-Real-IP \$remote_addr;
-  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  }
-  gzip on;
-  gzip_min_length 1000;
-  }
-upstream odoo {
-    server 127.0.0.1:8069 weight=1 fail_timeout=0;
+    access_log /var/log/nginx/testing-access.log;
+        error_log /var/log/nginx/testing-error.log;
+        proxy_buffers 16 64k;
+        proxy_buffer_size 128k;
+        client_max_body_size 4M;
+        gzip on;
+        gzip_min_length 1000;
+        gzip_disable "msie6";
+        gzip_vary on;
+        gzip_proxied any;
+        gzip_comp_level 6;
+        gzip_http_version 1.1;
+	gzip_types text/css
+		   text/javascript
+		   text/xml
+		   text/plain
+		   image/bmpg
+		   image/gif
+		   image/jpeg
+		   image/jpg
+                   image/png
+                   image/svg+xml
+                   image/x-icon
+                   application/javascript
+                   application/json
+                   application/rss+xml
+                   application/vnd.ms-fontobject
+                   application/x-font-ttf
+                   application/x-javascript
+                   application/xml
+                   application/xml+rss;
+
+
+        location / {
+        proxy_connect_timeout 3600;
+        proxy_read_timeout 3600;
+        proxy_send_timeout 3600;
+        send_timeout 3600;
+        proxy_pass http://odoosrv;
+        proxy_set_header Host $host:$server_port;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503;
+        proxy_redirect off;
+       }
+
+        location /longpolling {
+	proxy_pass http://odoolong;
+	proxy_set_header Host $host;
+	proxy_set_header X-Forwarded-Host $host;
+	proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+	proxy_set_header X-Forwarded-Proto $scheme;
+	proxy_set_header X-Real-IP $remote_addr;
+	proxy_next_upstream error timeout invalid_header http_500 http_502 http_503;
+	proxy_redirect off;
+        proxy_connect_timeout 3600;
+        proxy_read_timeout 3600;
+        proxy_send_timeout 3600;
+        send_timeout 3600;
+
+       }
 }
-upstream odoo-im {
-    server 127.0.0.1:8072 weight=1 fail_timeout=0;
-  }
+
 EOF
 
 ln -s /etc/nginx/sites-available/$dom /etc/nginx/sites-enabled/$dom
